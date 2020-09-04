0ce6b280db3864a20b4bd571f5bf9ac372dce92d
Vukasin <vikasinkurandic@gmail.com>
2020-09-04 20:18:26 +0200

    Initial commit

---
+++ js/form.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    $('.dugme__prijavi-se').click(function() {
+        let ime = $('#ime').val();
+        let prezime = $('#prezime').val();
+        let email = $('#email').val();
+        let godine = $('#godine').val();
+        let telefon = $('#telefon').val();
+        let pol = $('#pol').val();
+        let cilj = $('#cilj').val();
+        let plan = $('#plan__odabir').val();
+
+        if (!/^[A-Za-z\s]+$/.test(ime)) {
+            $('#ime_greska').removeClass('error-message-invisible').addClass('error-message-visible');
+            $('#ime').focus();
+
+        } else {
+            $('#ime_greska').removeClass('error-message-visible').addClass('error-message-invisible');
+
+
+            if (!/^[A-Za-z\s]+$/.test(prezime)) {
+                $('#prezime_greska').removeClass('error-message-invisible').addClass('error-message-visible');
+                $('#prezime').focus();
+            } else {
+                $('#prezime_greska').removeClass('error-message-visible').addClass('error-message-invisible');
+                if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                    $('#email_greska').removeClass('error-message-invisible').addClass('error-message-visible');
+                    $('#email').focus();
+                } else {
+                    $('#email_greska').removeClass('error-message-visible').addClass('error-message-invisible');
+                    if (godine == "" || godine < 16 || godine > 70) {
+                        $('#godine_greska').removeClass('error-message-invisible').addClass('error-message-visible');
+                        $('#godine').focus();
+                    } else {
+                        $('#godine_greska').removeClass('error-message-visible').addClass('error-message-invisible');
+                        if (telefon == "" || !/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(telefon)) {
+                            $('#telefon_greska').removeClass('error-message-invisible').addClass('error-message-visible');
+                            $('#telefon').focus();
+                        } else {
+                            $('#telefon_greska').removeClass('error-message-visible').addClass('error-message-invisible');
+                            //AJAX POST METHOD
+                            $.post('php/form.php', //url to php page
+                                {
+                                    ime: ime,
+                                    prezime: prezime,
+                                    email: email,
+                                    godine: godine,
+                                    telefon: telefon,
+                                    pol: pol,
+                                    cilj: cilj,
+                                    plan: plan
+                                }, // data to be submit
+                                function(data, textStatus, jqXHR) {
+                                    // success callback
+                                    console.log(data);
+                                    if (data === "succes") {
+                                        $('#php_greska').removeClass('error-message-visible').addClass('succes-message-visible');
+                                        $("#php_greska").html("<p>Uspesna prijava, hvala Vam</p>");
+                                    } else {
+                                        $("#php_greska").html(data);
+                                    }
+
+                                }
+
+                            );
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+    });
+});
---
+++ php/connection.php
@@ -0,0 +1,20 @@
+<?php  
+
+$host='localhost';
+
+$user='root';
+
+$password='';
+
+$database='nutri-logika_db';
+
+
+
+$conn= new mysqli($host,$user,$password,$database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
